452457b5297e4e0216eabe4d20ddea2a907ad861
Marly212 <brucelieber13@googlemail.com>
2020-02-11 15:12:12 +0100

    Innitialer Commit

--- Main.py
+++ Main.py
@@ -1,22 +1,36 @@
 import discord
 import logging
+import youtube_dl
+from discord.ext import commands
+from discord.utils import get
+import os
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.ERROR)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print("Iam Ready")
-
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
-
-        print("Nachricht von " + str(message.author) + "die enthält " + str(message.content))
+command_prefix = '.'
+bot = commands.Bot(command_prefix=command_prefix)
+Token = 'Njc0OTI3OTE1NDg1NjI2Mzc4.XkJ8fw.vwqz9XkeogSEwcTBmXPrRNbMLY0'
 
 
-client = MyClient()
-client.run("Njc0OTI3OTE1NDg1NjI2Mzc4.XjwhZA.HJ9ZZb4FxGalFOWzFjxBDHtDn8c")
+@bot.event
+async def on_ready():
+    print("Iam Ready")
+
+
+@bot.commands(pass_context=True)
+async def join(ctx):
+    voice = get(bot.voice_clients)
+    channel = ctx.message.author.voice.channel
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+
+    await voice.disconnect()
+
+bot.run(Token)
