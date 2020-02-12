17588976540b32ab0af4d10efc0faaa15ffd3ad0
Marly212 <brucelieber13@googlemail.com>
2020-02-12 16:48:04 +0100

    Innitialer Commit

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from discord.utils import get
 import os
 
+
 logger = logging.getLogger('discord')
 logger.setLevel(logging.ERROR)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
@@ -13,7 +14,7 @@ logger.addHandler(handler)
 
 command_prefix = '.'
 bot = commands.Bot(command_prefix=command_prefix)
-Token = 'Njc0OTI3OTE1NDg1NjI2Mzc4.XkJ8fw.vwqz9XkeogSEwcTBmXPrRNbMLY0'
+Token = 'Njc0OTI3OTE1NDg1NjI2Mzc4.XkQW3A.KjRs2EamXYPAt0PztfQcttezOYI'
 
 
 @bot.event
@@ -21,16 +22,10 @@ async def on_ready():
     print("Iam Ready")
 
 
-@bot.commands(pass_context=True)
+@bot.command(pass_context=True)
 async def join(ctx):
-    voice = get(bot.voice_clients)
-    channel = ctx.message.author.voice.channel
+    channel = ctx.message.author.voice.voice_channel
+    await bot.
 
-    if voice and voice.is_connected():
-        await voice.move_to(channel)
-    else:
-        voice = await channel.connect()
-
-    await voice.disconnect()
 
 bot.run(Token)
